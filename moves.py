c5a6abf4f476da76d3ac7ad13210c265de766025
PuercoPop <pirata@gmail.com>
2014-03-16 00:29:26 -0500

    Middle row renamed to center

--- moves.py
+++ moves.py
@@ -9,9 +9,9 @@ class Moves(object):
     top_right = 1
     top_middle = 2
     top_left = 3
-    middle_right = 4
-    middle_middle = 5
-    middle_left = 6
+    center_right = 4
+    center_middle = 5
+    center_left = 6
     bottom_right = 7
     bottom_middle = 8
     bottom_left = 9
