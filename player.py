f6231ca75f037f008e3d57d53013ac0bf394c954
PuercoPop <pirata@gmail.com>
2014-03-16 15:27:55 -0500

    Player read move from stdin

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from moves import Moves
 
 class Player(object):
     def __init__(self, name, symbol):
@@ -8,7 +9,12 @@ class Player(object):
     def move(self, board):
         """Should ensure a valid input.
         """
-        raise NotImplementedError
+        while True:
+            message = """
+            """.format()
+            input = raw_input(message)
+            if input in Moves:
+                return input
 
     def __repr__(self,):
         return self.name
